5e0339054fec0863a7bddf502d5583aa3f2d6b4a
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2022-08-03 15:28:17 +0800

    Initial commit

---
+++ simple-weather-ios-app/simple_weather_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_weather_ios_appApp.swift
+//  simple-weather-ios-app
+//
+//  Created by Adrian Alviento on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct simple_weather_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
